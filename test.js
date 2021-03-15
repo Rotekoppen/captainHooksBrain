d4584cd887e435ead50815d07d552fdca66399ff
Rotekoppen <33429716+Rotekoppen@users.noreply.github.com>
2021-03-15 16:31:50 +0100

    cleaned up

--- test.js
+++
@@ -1,9 +0,0 @@
-const nodeHtmlToImage = require('node-html-to-image')
-
-nodeHtmlToImage({
-  output: './image.png',
-  html: `
-
-`
-})
-  .then(() => console.log('The image was created successfully!'))
